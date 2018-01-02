44116c175ec0726647dfda234c949211037bf53a
Claus Brunner <claus.brunner@t-online.de>
2018-01-02 16:02:12 +0100

    init tm

---
+++ tm/controller/Main.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("de.brunner.tm.controller.Main", {
+
+		onInit: function () {
+
+		}
+	});
+});
